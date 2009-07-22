2dfb0c4d2c014f2f09b93ad7c0f466443ba487b4
Ben Green <greencommaben@gmail.com>
2009-07-21 21:56:19 -0500

    Giong to try and move this over to a premade physics engine, as my stuff is getting hairy. Box2D conversion started.

---
+++ Box2D/Examples/TestBed/Gen/fixed/Tests/TestEntries.d
@@ -0,0 +1,160 @@
+Gen/fixed/Tests/TestEntries.o: Tests/TestEntries.cpp \
+  Tests/../Framework/Test.h ../../Include/Box2D.h \
+  ../../Include/../Source/Common/b2Settings.h /usr/include/assert.h \
+  /usr/include/features.h /usr/include/sys/cdefs.h \
+  /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+  /usr/include/gnu/stubs-32.h /usr/include/math.h \
+  /usr/include/bits/huge_val.h /usr/include/bits/huge_valf.h \
+  /usr/include/bits/huge_vall.h /usr/include/bits/inf.h \
+  /usr/include/bits/nan.h /usr/include/bits/mathdef.h \
+  /usr/include/bits/mathcalls.h /usr/include/bits/mathinline.h \
+  ../../Include/../Source/Collision/Shapes/b2CircleShape.h \
+  ../../Include/../Source/Collision/Shapes/b2Shape.h \
+  ../../Include/../Source/Collision/Shapes/../../Common/b2Math.h \
+  ../../Include/../Source/Collision/Shapes/../../Common/b2Settings.h \
+  /usr/include/c++/4.3/cmath \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/c++config.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/os_defines.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/cpu_defines.h \
+  /usr/include/c++/4.3/bits/cpp_type_traits.h \
+  /usr/include/c++/4.3/ext/type_traits.h \
+  /usr/include/c++/4.3/bits/cmath.tcc /usr/include/c++/4.3/cfloat \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/float.h \
+  /usr/include/c++/4.3/cstdlib /usr/include/c++/4.3/cstddef \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/stddef.h \
+  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+  /usr/include/bits/waitstatus.h /usr/include/endian.h \
+  /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+  /usr/include/xlocale.h /usr/include/sys/types.h \
+  /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+  /usr/include/time.h /usr/include/sys/select.h \
+  /usr/include/bits/select.h /usr/include/bits/sigset.h \
+  /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
+  /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
+  /usr/include/bits/stdlib.h /usr/include/stdio.h /usr/include/libio.h \
+  /usr/include/_G_config.h /usr/include/wchar.h \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/stdarg.h \
+  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+  /usr/include/bits/stdio.h /usr/include/bits/stdio2.h \
+  ../../Include/../Source/Collision/Shapes/../b2Collision.h \
+  ../../Include/../Source/Collision/Shapes/../../Common/b2Math.h \
+  /usr/include/c++/4.3/climits \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include-fixed/limits.h \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include-fixed/syslimits.h \
+  /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+  /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+  /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+  ../../Include/../Source/Collision/Shapes/b2PolygonShape.h \
+  ../../Include/../Source/Collision/b2BroadPhase.h \
+  ../../Include/../Source/Collision/../Common/b2Settings.h \
+  ../../Include/../Source/Collision/b2Collision.h \
+  ../../Include/../Source/Collision/b2PairManager.h \
+  ../../Include/../Source/Collision/../Common/b2Math.h \
+  ../../Include/../Source/Dynamics/b2WorldCallbacks.h \
+  ../../Include/../Source/Dynamics/../Common/b2Settings.h \
+  ../../Include/../Source/Dynamics/b2World.h \
+  ../../Include/../Source/Dynamics/../Common/b2Math.h \
+  ../../Include/../Source/Dynamics/../Common/b2BlockAllocator.h \
+  ../../Include/../Source/Dynamics/../Common/b2Settings.h \
+  ../../Include/../Source/Dynamics/../Common/b2StackAllocator.h \
+  ../../Include/../Source/Dynamics/b2ContactManager.h \
+  ../../Include/../Source/Dynamics/../Collision/b2BroadPhase.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/b2NullContact.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/../../Common/b2Math.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/b2Contact.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/../../Collision/b2Collision.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/../../Collision/Shapes/b2Shape.h \
+  ../../Include/../Source/Dynamics/b2WorldCallbacks.h \
+  ../../Include/../Source/Dynamics/b2Body.h \
+  ../../Include/../Source/Dynamics/../Collision/Shapes/b2Shape.h \
+  ../../Include/../Source/Dynamics/Joints/b2Joint.h \
+  ../../Include/../Source/Dynamics/Joints/../../Common/b2Math.h \
+  /usr/include/c++/4.3/memory /usr/include/c++/4.3/bits/stl_algobase.h \
+  /usr/include/c++/4.3/bits/functexcept.h \
+  /usr/include/c++/4.3/exception_defines.h \
+  /usr/include/c++/4.3/ext/numeric_traits.h \
+  /usr/include/c++/4.3/bits/stl_pair.h \
+  /usr/include/c++/4.3/bits/stl_move.h \
+  /usr/include/c++/4.3/bits/concept_check.h \
+  /usr/include/c++/4.3/bits/stl_iterator_base_types.h \
+  /usr/include/c++/4.3/bits/stl_iterator_base_funcs.h \
+  /usr/include/c++/4.3/bits/stl_iterator.h \
+  /usr/include/c++/4.3/debug/debug.h \
+  /usr/include/c++/4.3/bits/allocator.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/c++allocator.h \
+  /usr/include/c++/4.3/ext/new_allocator.h /usr/include/c++/4.3/new \
+  /usr/include/c++/4.3/exception \
+  /usr/include/c++/4.3/bits/stl_construct.h \
+  /usr/include/c++/4.3/bits/stl_uninitialized.h \
+  /usr/include/c++/4.3/bits/stl_tempbuf.h \
+  /usr/include/c++/4.3/bits/stl_raw_storage_iter.h \
+  /usr/include/c++/4.3/backward/auto_ptr.h \
+  ../../Include/../Source/Dynamics/Contacts/b2Contact.h \
+  ../../Include/../Source/Dynamics/Joints/b2DistanceJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2Joint.h \
+  ../../Include/../Source/Dynamics/Joints/b2MouseJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2PrismaticJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2RevoluteJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2PulleyJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2GearJoint.h \
+  Tests/../Framework/Render.h Tests/../Framework/Render.h \
+  ../../Contrib/freeglut/GL/glut.h ../../Contrib/freeglut/GL/freeglut.h \
+  ../../Contrib/freeglut/GL/freeglut_std.h /usr/include/GL/gl.h \
+  /usr/include/GL/glext.h /usr/include/inttypes.h /usr/include/stdint.h \
+  /usr/include/bits/wchar.h /usr/include/GL/glu.h \
+  ../../Contrib/freeglut/GL/freeglut_ext.h Tests/ApplyForce.h \
+  Tests/BipedTest.h Tests/Biped.h Tests/Bridge.h Tests/BroadPhaseTest.h \
+  Tests/Car.h Tests/CCDTest.h Tests/Chain.h Tests/ContactCallbackTest.h \
+  /usr/include/c++/4.3/set /usr/include/c++/4.3/bits/stl_tree.h \
+  /usr/include/c++/4.3/bits/stl_function.h \
+  /usr/include/c++/4.3/backward/binders.h \
+  /usr/include/c++/4.3/bits/stl_set.h \
+  /usr/include/c++/4.3/bits/stl_multiset.h /usr/include/c++/4.3/deque \
+  /usr/include/c++/4.3/bits/stl_deque.h \
+  /usr/include/c++/4.3/bits/deque.tcc /usr/include/c++/4.3/sstream \
+  /usr/include/c++/4.3/istream /usr/include/c++/4.3/ios \
+  /usr/include/c++/4.3/iosfwd /usr/include/c++/4.3/bits/stringfwd.h \
+  /usr/include/c++/4.3/bits/postypes.h /usr/include/c++/4.3/cwchar \
+  /usr/include/bits/wchar2.h /usr/include/c++/4.3/bits/char_traits.h \
+  /usr/include/c++/4.3/cstdio /usr/include/c++/4.3/bits/localefwd.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/c++locale.h \
+  /usr/include/c++/4.3/clocale /usr/include/locale.h \
+  /usr/include/bits/locale.h /usr/include/c++/4.3/cctype \
+  /usr/include/ctype.h /usr/include/c++/4.3/bits/ios_base.h \
+  /usr/include/c++/4.3/ext/atomicity.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/gthr.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/gthr-default.h \
+  /usr/include/pthread.h /usr/include/sched.h /usr/include/bits/sched.h \
+  /usr/include/signal.h /usr/include/bits/setjmp.h /usr/include/unistd.h \
+  /usr/include/bits/posix_opt.h /usr/include/bits/environments.h \
+  /usr/include/bits/confname.h /usr/include/getopt.h \
+  /usr/include/bits/unistd.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/atomic_word.h \
+  /usr/include/c++/4.3/bits/locale_classes.h /usr/include/c++/4.3/string \
+  /usr/include/c++/4.3/bits/ostream_insert.h \
+  /usr/include/c++/4.3/cxxabi-forced.h \
+  /usr/include/c++/4.3/bits/basic_string.h \
+  /usr/include/c++/4.3/bits/basic_string.tcc \
+  /usr/include/c++/4.3/bits/locale_classes.tcc \
+  /usr/include/c++/4.3/streambuf /usr/include/c++/4.3/bits/streambuf.tcc \
+  /usr/include/c++/4.3/bits/basic_ios.h \
+  /usr/include/c++/4.3/bits/locale_facets.h /usr/include/c++/4.3/cwctype \
+  /usr/include/wctype.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/ctype_base.h \
+  /usr/include/c++/4.3/bits/streambuf_iterator.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/ctype_inline.h \
+  /usr/include/c++/4.3/bits/locale_facets.tcc \
+  /usr/include/c++/4.3/bits/basic_ios.tcc /usr/include/c++/4.3/ostream \
+  /usr/include/c++/4.3/bits/ostream.tcc \
+  /usr/include/c++/4.3/bits/istream.tcc \
+  /usr/include/c++/4.3/bits/sstream.tcc /usr/include/c++/4.3/iostream \
+  Tests/CollisionFiltering.h Tests/CollisionProcessing.h \
+  /usr/include/c++/4.3/algorithm /usr/include/c++/4.3/bits/stl_algo.h \
+  /usr/include/c++/4.3/bits/algorithmfwd.h \
+  /usr/include/c++/4.3/bits/stl_heap.h Tests/CompoundShapes.h \
+  Tests/DistanceTest.h Tests/Dominos.h Tests/ElasticBody.h Tests/Gears.h \
+  Tests/MotorsAndLimits.h Tests/PolyCollision.h Tests/PolyShapes.h \
+  Tests/Pulleys.h Tests/Pyramid.h Tests/SensorTest.h Tests/ShapeEditing.h \
+  Tests/SliderCrank.h Tests/TheoJansen.h Tests/TimeOfImpact.h \
+  Tests/VaryingFriction.h Tests/VaryingRestitution.h \
+  Tests/VerticalStack.h Tests/Web.h
---
+++ Box2D/Examples/TestBed/Gen/float/Tests/Biped.d
@@ -0,0 +1,98 @@
+Gen/float/Tests/Biped.o: Tests/Biped.cpp Tests/Biped.h \
+  ../../Include/Box2D.h ../../Include/../Source/Common/b2Settings.h \
+  /usr/include/assert.h /usr/include/features.h /usr/include/sys/cdefs.h \
+  /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+  /usr/include/gnu/stubs-32.h /usr/include/math.h \
+  /usr/include/bits/huge_val.h /usr/include/bits/huge_valf.h \
+  /usr/include/bits/huge_vall.h /usr/include/bits/inf.h \
+  /usr/include/bits/nan.h /usr/include/bits/mathdef.h \
+  /usr/include/bits/mathcalls.h /usr/include/bits/mathinline.h \
+  ../../Include/../Source/Collision/Shapes/b2CircleShape.h \
+  ../../Include/../Source/Collision/Shapes/b2Shape.h \
+  ../../Include/../Source/Collision/Shapes/../../Common/b2Math.h \
+  ../../Include/../Source/Collision/Shapes/../../Common/b2Settings.h \
+  /usr/include/c++/4.3/cmath \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/c++config.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/os_defines.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/cpu_defines.h \
+  /usr/include/c++/4.3/bits/cpp_type_traits.h \
+  /usr/include/c++/4.3/ext/type_traits.h \
+  /usr/include/c++/4.3/bits/cmath.tcc /usr/include/c++/4.3/cfloat \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/float.h \
+  /usr/include/c++/4.3/cstdlib /usr/include/c++/4.3/cstddef \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/stddef.h \
+  /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+  /usr/include/bits/waitstatus.h /usr/include/endian.h \
+  /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+  /usr/include/xlocale.h /usr/include/sys/types.h \
+  /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+  /usr/include/time.h /usr/include/sys/select.h \
+  /usr/include/bits/select.h /usr/include/bits/sigset.h \
+  /usr/include/bits/time.h /usr/include/sys/sysmacros.h \
+  /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
+  /usr/include/bits/stdlib.h /usr/include/stdio.h /usr/include/libio.h \
+  /usr/include/_G_config.h /usr/include/wchar.h \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include/stdarg.h \
+  /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+  /usr/include/bits/stdio.h /usr/include/bits/stdio2.h \
+  ../../Include/../Source/Collision/Shapes/../b2Collision.h \
+  ../../Include/../Source/Collision/Shapes/../../Common/b2Math.h \
+  /usr/include/c++/4.3/climits \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include-fixed/limits.h \
+  /usr/lib/gcc/i486-linux-gnu/4.3.3/include-fixed/syslimits.h \
+  /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+  /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+  /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+  ../../Include/../Source/Collision/Shapes/b2PolygonShape.h \
+  ../../Include/../Source/Collision/b2BroadPhase.h \
+  ../../Include/../Source/Collision/../Common/b2Settings.h \
+  ../../Include/../Source/Collision/b2Collision.h \
+  ../../Include/../Source/Collision/b2PairManager.h \
+  ../../Include/../Source/Collision/../Common/b2Math.h \
+  ../../Include/../Source/Dynamics/b2WorldCallbacks.h \
+  ../../Include/../Source/Dynamics/../Common/b2Settings.h \
+  ../../Include/../Source/Dynamics/b2World.h \
+  ../../Include/../Source/Dynamics/../Common/b2Math.h \
+  ../../Include/../Source/Dynamics/../Common/b2BlockAllocator.h \
+  ../../Include/../Source/Dynamics/../Common/b2Settings.h \
+  ../../Include/../Source/Dynamics/../Common/b2StackAllocator.h \
+  ../../Include/../Source/Dynamics/b2ContactManager.h \
+  ../../Include/../Source/Dynamics/../Collision/b2BroadPhase.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/b2NullContact.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/../../Common/b2Math.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/b2Contact.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/../../Collision/b2Collision.h \
+  ../../Include/../Source/Dynamics/../Dynamics/Contacts/../../Collision/Shapes/b2Shape.h \
+  ../../Include/../Source/Dynamics/b2WorldCallbacks.h \
+  ../../Include/../Source/Dynamics/b2Body.h \
+  ../../Include/../Source/Dynamics/../Collision/Shapes/b2Shape.h \
+  ../../Include/../Source/Dynamics/Joints/b2Joint.h \
+  ../../Include/../Source/Dynamics/Joints/../../Common/b2Math.h \
+  /usr/include/c++/4.3/memory /usr/include/c++/4.3/bits/stl_algobase.h \
+  /usr/include/c++/4.3/bits/functexcept.h \
+  /usr/include/c++/4.3/exception_defines.h \
+  /usr/include/c++/4.3/ext/numeric_traits.h \
+  /usr/include/c++/4.3/bits/stl_pair.h \
+  /usr/include/c++/4.3/bits/stl_move.h \
+  /usr/include/c++/4.3/bits/concept_check.h \
+  /usr/include/c++/4.3/bits/stl_iterator_base_types.h \
+  /usr/include/c++/4.3/bits/stl_iterator_base_funcs.h \
+  /usr/include/c++/4.3/bits/stl_iterator.h \
+  /usr/include/c++/4.3/debug/debug.h \
+  /usr/include/c++/4.3/bits/allocator.h \
+  /usr/include/c++/4.3/i486-linux-gnu/bits/c++allocator.h \
+  /usr/include/c++/4.3/ext/new_allocator.h /usr/include/c++/4.3/new \
+  /usr/include/c++/4.3/exception \
+  /usr/include/c++/4.3/bits/stl_construct.h \
+  /usr/include/c++/4.3/bits/stl_uninitialized.h \
+  /usr/include/c++/4.3/bits/stl_tempbuf.h \
+  /usr/include/c++/4.3/bits/stl_raw_storage_iter.h \
+  /usr/include/c++/4.3/backward/auto_ptr.h \
+  ../../Include/../Source/Dynamics/Contacts/b2Contact.h \
+  ../../Include/../Source/Dynamics/Joints/b2DistanceJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2Joint.h \
+  ../../Include/../Source/Dynamics/Joints/b2MouseJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2PrismaticJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2RevoluteJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2PulleyJoint.h \
+  ../../Include/../Source/Dynamics/Joints/b2GearJoint.h Tests/BipedDef.h
